ff6eb22d84300e028c5648c6a4d9fddbd8c0260d
Marco Antonio <marcoantoniosilvanunes10@gmail.com>
2023-03-22 21:35:03 -0300

    Commit

---
+++ WorkerFuncionariosIntranet/Service1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+using WorkerFuncionariosIntranet.Services;
+
+namespace WorkerFuncionariosIntranet
+{
+    public partial class Service1 : ServiceBase
+    {
+        int intervalo = Convert.ToInt32(ConfigurationManager.AppSettings["timer"].ToString());
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread thread = new Thread(Start);
+            //Start(null);
+            thread.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected void Start(object sender)
+        {
+            while (true)
+            {
+
+                string _procedure = "DBO.SP_COR_SEL_FuncionariosIntranet";
+                string _caminhotxt = ConfigurationManager.AppSettings["path"].ToString();
+
+                //1 Chamar a Procedure
+                var myDataSet = new DataSet();
+                myDataSet = SelectRowsProc(myDataSet, _procedure);
+
+                string _txtFile = "";
+                foreach (DataRow table in myDataSet.Tables[0].Rows)
+                {
+                    _txtFile = _txtFile + table[0].ToString();
+                }
+                File.WriteAllText(_caminhotxt, _txtFile);
+                Thread.Sleep(intervalo);
+            }
+        }
+
+        private DataSet SelectRowsProc(DataSet dataSet, string proc)
+        {
+            using (var conn = SingleConnection.GetConnection())
+            {
+
+                using (var adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand(proc, (SqlConnection)conn);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    adapter.SelectCommand.CommandTimeout = 180000;
+                    adapter.Fill(dataSet);
+                    SingleConnection.CloseConnection();
+                    return dataSet;
+                }
+            };
+        }
+    }
+}
---
+++ WorkerFuncionariosIntranet/Services/SingleConnection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerFuncionariosIntranet.Services
+{
+    class SingleConnection
+    {
+
+        private static SqlConnection Con = null;
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                string ConnectionString = ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                if (Con != null)
+                {
+                    if (Con.State != ConnectionState.Open)
+                    {
+                        Con = new SqlConnection(ConnectionString);
+                        Con.Open();
+
+                        return Con;
+                    }
+
+                    else return Con;
+                }
+
+                Con = new SqlConnection(ConnectionString);
+                Con.Open();
+                return Con;
+            }
+            catch (Exception e)
+            {
+                EventLog InsereLog = new EventLog();
+                InsereLog.Source = "GetConnection Woker";
+                InsereLog.WriteEntry(e.Message);
+                InsereLog.Dispose();
+
+                return Con;
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (Con.State == ConnectionState.Open)
+                {
+                    Con.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                EventLog InsereLog = new EventLog();
+                InsereLog.Source = "CloseConnection Woker";
+                InsereLog.WriteEntry(e.Message);
+                InsereLog.Dispose();
+            }
+        }
+    }
+
+}
+
